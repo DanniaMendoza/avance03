364d80ec1f03737d47dd138ff3f4df829795cb6f
DanniaMendoza <u18305485@utp.edu.pe>
2021-05-25 20:57:44 -0500

    tercer avance

---
+++ Tercer Avance/js/mitos.js
@@ -0,0 +1,38 @@
+
+function mito1(){
+    swal("Falso!", 
+    "NO. La vacuna contra la influenza no brinda protección contra el nuevo coronavirus, este es tan nuevo y diferente que necesita su propia vacuna. Aunque la vacuna contra la influenza no es efectiva para el COVID-19, vacunarse es muy recomendable para proteger la salud."
+    , "error");
+}
+
+function mito2(){
+    swal("Falso!", 
+    "En realidad uno debe de lavarse las manos"
+    , "error");
+}
+
+function mito3(){
+    swal("Falso!", 
+    "En realidad uno debe de lavarse las manos"
+    , "error");
+}
+
+function mito4(){
+    swal("Falso!", 
+    "En realidad uno debe de lavarse las manos"
+    , "error");
+}
+
+function mito5(){
+    swal("Falso!", 
+    "En realidad uno debe de lavarse las manos"
+    , "error");
+}
+
+function mito6(){
+    swal("Falso!", 
+    "En realidad uno debe de lavarse las manos"
+    , "error");
+}
+
+
